597c985db5f4de006319ea49fa53e1ff5c4cd9d1
pabloangusto <pabloangusto@gmail.com>
2023-05-02 19:34:22 +0200

    .

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -56,30 +56,32 @@ GROUP BY C2.Nombre)
                                                                         GROUP BY C.Nombre)
 
 --Fin 1:
-Select C3.Nombre, retrasos*100/total
+Select C3.Nombre, retrasos*100/total as ratio
 FROM (Select C2.Nombre as nombreT, count(*) as total
         FROM COMPANIAS C2, VUELOS V2, AVIONES A2
         WHERE V2.avion = A2.matricula and A2.compania = C2.codigo and C2.Nombre IN(Select C.Nombre
-                                                                                FROM COMPANIAS C, VUELOS V, AVIONES A, INCIDENTES I, RETRASOS R
-                                                                                WHERE V.avion = A.matricula and A.compania = C.codigo and I.vuelo = V.IdVuelo and I.IdIncidente = R.IdIncidente
+                                                                                FROM COMPANIAS C, VUELOS V, AVIONES A, RETRASOS R
+                                                                                WHERE V.avion = A.matricula and A.compania = C.codigo and R.vuelo = V.IdVuelo
                                                                                 GROUP BY C.Nombre)
         GROUP BY C2.Nombre) X, (Select C.Nombre as nombreR, count(*) as retrasos
-                                                                                FROM COMPANIAS C, VUELOS V, AVIONES A, INCIDENTES I, RETRASOS R
-                                                                                WHERE V.avion = A.matricula and A.compania = C.codigo and I.vuelo = V.IdVuelo and I.IdIncidente = R.IdIncidente
+                                                                                FROM COMPANIAS C, VUELOS V, AVIONES A,  RETRASOS R
+                                                                                WHERE V.avion = A.matricula and A.compania = C.codigo and R.vuelo = V.IdVuelo 
                                                                                 GROUP BY C.Nombre) Z, COMPANIAS C3
 WHERE C3.Nombre = nombreR and C3.Nombre = nombreT
+ORDER BY ratio DESC
 
 
 
-Select M2.fabricante
+
+Select M2.Modelo, M2.fabricante, M2.Motor
 FROM MODELO M2
 WHERE (Select count(*)*100
-        FROM VUELOS V, AVIONES A2, MODELO M, INCIDENTES I, RETRASOS R, CAUSAS CA
-        WHERE V.Avion= A2.matricula and A2.modelo=M.modelo and M.fabricante=M2.fabricante and V.IdVuelo=I.Vuelo and I.IdIncidente=R.IdIncidente and R.IdIncidente=CA.IdIncidente and CA.Causa='seguridad'
-        GROUP BY M.fabricante)>(Select count(*)
-                                                                                                                                                        FROM VUELOS V, AVIONES A2, MODELO M, INCIDENTES I, RETRASOS R
-                                                                                                                                                        WHERE V.Avion=A2.matricula and A2.modelo=M.modelo and M.fabricante=M2.fabricante and V.IdVuelo=I.vuelo and I.IdIncidente=R.IdIncidente
-                                                                                                                                                        GROUP BY M.fabricante)
+        FROM VUELOS V, AVIONES A2, MODELO M, RETRASOS R
+        WHERE V.Avion= A2.matricula and A2.modelo=M2.modelo and V.IdVuelo=R.Vuelo and R.causa="seguridad"
+        GROUP BY M.modelo)>(Select count(*)
+                               FROM VUELOS V, AVIONES A2, MODELO M, RETRASOS R
+                               WHERE V.Avion=A2.matricula and A2.modelo=M2.modelo and V.IdVuelo=R.vuelo
+                               GROUP BY M.fabricante)
 
 
 
@@ -93,6 +95,7 @@ WHERE (Select count(*)*100
 
 
 
-Select M.fabricante, count(*)
+Select M.modelo, M.fabricante, M.motor, count(*)
         FROM VUELOS V, AVIONES A2, MODELO M, INCIDENTES I, RETRASOS R, CAUSAS CA
         WHERE V.Avion= A2.matricula and A2.modelo=M.modelo and V.IdVuelo=I.Vuelo and I.IdIncidente=R.IdIncidente and R.IdIncidente=CA.IdIncidente and CA.Causa='seguridad'
+GROUP BY M.modelo, M.fabricante, M.motor
