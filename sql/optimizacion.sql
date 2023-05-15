41e77c290f1c89b24152752b0ed719fa526183e0
Andreiiii19 <97831622+Andreiiii19@users.noreply.github.com>
2023-05-15 22:55:22 +0200

    opt terminada

--- sql/optimizacion.sql
+++ sql/optimizacion.sql
@@ -1,3 +1,13 @@
+-----1
+CREATE INDEX vuelos_idx ON VUELOS(idVuelo, Avion);
+
+CREATE INDEX aviones_idx ON AVIONES(Matricula, Compania);
+DROP INDEX vuelos_idx ;
+DROP INDEX aviones_idx ;
+
+
+
+-----2
 CREATE MATERIALIZED VIEW retardos_seg AS
 Select count(*)*100 as ret_seg, M.Modelo as mod
         FROM VUELOS V, AVIONES A2, MODELO M, RETRASOS R
@@ -21,27 +31,7 @@ DROP  MATERIALIZED VIEW retardos_seg;
 DROP MATERIALIZED VIEW retardos;
 
 
-CREATE INDEX vuelos_idx ON VUELOS(idVuelo, Avion);
-
-CREATE INDEX aviones_idx ON AVIONES(Matricula, Compania);
-DROP INDEX vuelos_idx ;
-DROP INDEX aviones_idx ;
-
 -----3
-WITH
-        X as (Select A.IATA as aeropuerto,V.FechSalida as fecha, count(*) as num
-                        FROM VUELOS V, VUELOS V2, AEROPUERTOS A
-                        WHERE V.AeropuertoO = A.IATA  and ((V2.AeropuertoO = A.IATA and V.FechSalida + 15/1440 >= V2.FechSalida and V.FechSalida - 15/1440 <= V2.FechSalida) or (V2.AeropuertoD = A.IATA and V.FechSalida + 15/1440 >= V2.FechLlegada and V.FechSalida - 15/1440<= V2.FechLlegada))
-                        GROUP BY V.IdVuelo, A.IATA, V.FechSalida
-                        ORDER BY num),
-        Z as (Select max(num) AS maximo
-        FROM  X)
-Select X.aeropuerto, TO_CHAR(X.fecha, 'YYYY-MM-DD hh:mi:ss') as fechafin, X.num
-FROM     Z,  X
-WHERE X.num=maximo
-GROUP BY X.aeropuerto, X.fecha, X.num
-
-
 CREATE INDEX vuelos_idx2 ON VUELOS(FechSalida,FechLlegada,idVuelo,AeropuertoD,AeropuertoO);
 DROP INDEX vuelos_idx2 ;
 
