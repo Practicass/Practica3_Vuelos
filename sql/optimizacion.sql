0a291aa9db086e92bf50ece892deb8cf81e053e4
Miguel Aréjula Aisa <arejula10@gmail.com>
2023-05-15 19:34:40 +0200

    3 optimizada

--- sql/optimizacion.sql
+++ sql/optimizacion.sql
@@ -20,3 +20,49 @@ WHERE   ret_seg > ret and M2.Modelo = R.Mod and RE.Modelo=R.Mod;
 CREATE INDEX vuelos_idx ON (idVuelo, Avion);
 
 CREATE INDEX aviones_idx ON (Matricula, Compania);
+DROP INDEX vuelos_idx ;
+DROP INDEX aviones_idx ;
+
+-----3
+WITH
+        X as (Select A.IATA as aeropuerto,V.FechSalida as fecha, count(*) as num
+                        FROM VUELOS V, VUELOS V2, AEROPUERTOS A
+                        WHERE V.AeropuertoO = A.IATA  and ((V2.AeropuertoO = A.IATA and V.FechSalida + 15/1440 >= V2.FechSalida and V.FechSalida - 15/1440 <= V2.FechSalida) or (V2.AeropuertoD = A.IATA and V.FechSalida + 15/1440 >= V2.FechLlegada and V.FechSalida - 15/1440<= V2.FechLlegada))
+                        GROUP BY V.IdVuelo, A.IATA, V.FechSalida
+                        ORDER BY num),
+        Z as (Select max(num) AS maximo
+        FROM  X)
+Select X.aeropuerto, TO_CHAR(X.fecha, 'YYYY-MM-DD hh:mi:ss') as fechafin, X.num
+FROM     Z,  X
+WHERE X.num=maximo
+GROUP BY X.aeropuerto, X.fecha, X.num
+
+
+CREATE INDEX vuelos_idx2 ON VUELOS(FechSalida,FechLlegada,idVuelo,AeropuertoD,AeropuertoO);
+DROP INDEX vuelos_idx2 ;
+
+
+CREATE MATERIALIZED VIEW actividad AS
+Select A.IATA as aeropuerto,V.FechSalida as fecha, count(*) as num
+                        FROM VUELOS V, VUELOS V2, AEROPUERTOS A
+                        WHERE V.AeropuertoO = A.IATA  and ((V2.AeropuertoO = A.IATA and V.FechSalida + 15/1440 >= V2.FechSalida and V.FechSalida - 15/1440 <= V2.FechSalida) or (V2.AeropuertoD = A.IATA and V.FechSalida + 15/1440 >= V2.FechLlegada and V.FechSalida - 15/1440<= V2.FechLlegada))
+                        GROUP BY V.IdVuelo, A.IATA, V.FechSalida
+                        ORDER BY num;
+
+
+--op1
+WITH
+        Z as (Select max(actividad.num) AS maximo
+        FROM  actividad)
+ Select actividad.aeropuerto, TO_CHAR(actividad.fecha, 'YYYY-MM-DD hh:mi:ss') as fechafin, actividad.num
+FROM     Z,actividad
+WHERE actividad.num=maximo
+GROUP BY actividad.aeropuerto, actividad.fecha, actividad.num;
+
+--op2
+ Select actividad.aeropuerto, TO_CHAR(actividad.fecha, 'YYYY-MM-DD hh:mi:ss') as fechafin, actividad.num
+FROM     actividad
+WHERE actividad.num=(Select max(actividad.num) AS maximo
+        FROM  actividad)
+GROUP BY actividad.aeropuerto, actividad.fecha, actividad.num;
+                       
